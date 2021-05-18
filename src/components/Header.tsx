cf35ac8c1f233c12939e5046a8851124713acea5
Adam Chwedyk <a.chwedyk@toptal.com>
2021-05-19 00:59:28 +0200

    Use styled for some components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
 
 interface HeaderProps {
   title: string
 }
 
-export const Header = ({title}: HeaderProps) => <header className="App-header">{title}</header>
+const HeaderWrapper = styled.header`
+  background-color: #55cbcd;
+  min-height: 10vh;
+  max-height: 20vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(12px + 2vmin);
+  color: black;
+`
+
+export const Header = ({title}: HeaderProps) => <HeaderWrapper>{title}</HeaderWrapper>
--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import styled from 'styled-components';
 
 interface MessageProps {
   label: string
 }
 
-export const Message = ({label}: MessageProps) => <div className="Message">{label}</div>
+export const Wrapper = styled.div`
+  margin: 16px;
+`
+
+export const Message = ({label}: MessageProps) => <Wrapper>{label}</Wrapper>
